0c02aada0b993c5dcfacb9a39e8d9909f4bda294
Wahaha <Wahaha@DESKTOP-DGCG1BT>
2019-06-13 21:37:22 +0300

    Changed password model

--- DataAccessLayer/Entities/User.cs
+++ DataAccessLayer/Entities/User.cs
@@ -17,36 +17,9 @@ namespace DataAccessLayer.Models
 		[RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
 		public string Email { get; set; }
 
-		private string _password;
-		[MaxLength(64)]
+		[MaxLength(256)]
 		[Required(AllowEmptyStrings = false)]
-		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$")]
-		public string Sha256Password
-		{
-			get
-			{
-				return _password;
-			}
-			set
-			{
-				_password = ComputeSha256Hash(value);
-			}
-		}
-		
-		static string ComputeSha256Hash(string rawData)
-		{
-			using (SHA256 sha256Hash = SHA256.Create())
-			{
-				byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
-
-				StringBuilder builder = new StringBuilder();
-				for (int i = 0; i < bytes.Length; i++)
-				{
-					builder.Append(bytes[i].ToString("x2"));
-				}
-				return builder.ToString();
-			}
-		}
+		public string Sha256Password { get; set; }
 
 		[ForeignKey("Profile")]
 		public int ProfileId { get; set; }
